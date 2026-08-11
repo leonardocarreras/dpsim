d54278e576383864420f57bdf6c50909be0cdb1a
Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>
2026-08-11 14:42:30 +0200

    chore: restore the MPL-2.0 headers dropped in two 2022 commits
    Signed-off-by: Leonardo Carreras <leonardo.carreras@eonerc.rwth-aachen.de>

--- dpsim/include/dpsim/Simulation.h
+++ dpsim/include/dpsim/Simulation.h
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2017-2021 Institute for Automation of Complex Power Systems, EONERC, RWTH Aachen University
+// SPDX-License-Identifier: MPL-2.0
 
 #pragma once
 
